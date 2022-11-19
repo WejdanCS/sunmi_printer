bb743728aea73f9c8a57b07fab6a2f96d66a84e5
Do Manh Tai <manhtai831@gmail.com>
2022-11-19 14:29:59 +0700

    update

--- android/src/main/java/com/wojoodtech/flutter_sunmi_printer_plus/utils/SunmiPrintHelper.java
+++ android/src/main/java/com/wojoodtech/flutter_sunmi_printer_plus/utils/SunmiPrintHelper.java
@@ -90,7 +90,7 @@ public class SunmiPrintHelper {
 
 
     public void setFontSize(float fontSize) throws Exception {
-        if (isUnbindingPrinter()) return;
+//        if (isUnbindingPrinter()) return;
         try {
             sunmiPrinterService.setFontSize(fontSize, null);
         } catch (RemoteException e) {
@@ -100,7 +100,7 @@ public class SunmiPrintHelper {
 
 
     public void setBold(boolean isBold) {
-        if (isUnbindingPrinter()) return;
+//        if (isUnbindingPrinter()) return;
 
         try {
 
